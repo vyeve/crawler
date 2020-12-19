672dbabebee61b2c8598ccf2fbc51f3ac4a6df0f
vyeve <xobpaxa@ukr.net>
2020-12-19 23:45:00 +0200

    added example

---
+++ example/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/vyeve/crawler"
+)
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+func main() {
+	siteName := "https://github.com/"
+
+	tn := time.Now()
+	defer func() {
+		fmt.Printf("\n\tTotal time: %s\n", time.Since(tn))
+	}()
+	f, err := os.OpenFile("result.yaml", os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	if err = f.Truncate(0); err != nil {
+		panic(err)
+	}
+	if _, err = f.Seek(0, 0); err != nil {
+		panic(err)
+	}
+	st, err := crawler.New(siteName)
+	if err != nil {
+		panic(err)
+	}
+	st.Crawl().Print(f)
+}
