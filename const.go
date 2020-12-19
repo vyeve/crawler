e5672e4e1a9d7676f6fa91f173d5806eb00b8220
vyeve <xobpaxa@ukr.net>
2020-12-19 23:34:57 +0200

    added list of supported attributes

---
+++ const.go
@@ -0,0 +1,30 @@
+package crawler
+
+const (
+	httpKey  = "http://"
+	httpsKey = "https://"
+	wwwKey   = "www."
+)
+
+// List of HTML tag attributes which have a URL value:
+// https://stackoverflow.com/questions/2725156/complete-list-of-html-tag-attributes-which-have-a-url-value
+var htmlElements = map[string][]string{
+	"a":          {"href"},
+	"applet":     {"codebase"},
+	"area":       {"href"},
+	"base":       {"href"},
+	"blockquote": {"cite"},
+	"body":       {"background"},
+	"del":        {"cite"},
+	"form":       {"action"},
+	"frame":      {"longdesc", "src"},
+	"head":       {"profile"},
+	"iframe":     {"longdesc", "src"},
+	"img":        {"longdesc", "src", "usemap"},
+	"input":      {"src", "usemap"},
+	"ins":        {"cite"},
+	"link":       {"href"},
+	"object":     {"classid", "codebase", "data", "usemap"},
+	"q":          {"cite"},
+	"script":     {"src"},
+}
